b50a7eb55038959df5c5043254c60fb10ae3dbe0
Aleksei Leontev <aleksei_leontev94@mail.ru>
2024-05-03 19:44:08 +0500

    selenide

--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -24,11 +24,11 @@ public class AppCardDeliveryTest {
     @Test
     public void shouldTestSuccessRegistrationForm() {
         Selenide.open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Петропавловск-Камчатский");
+        $("[data-test-id='city'] input").setValue("Югорск");
         String planningDate = generateDate(3, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").setValue("17.02.2024");
-        $("[data-test-id='name'] input").setValue("Гаращенко Любовь");
-        $("[data-test-id='phone'] input").setValue("+79990005544");
+        $("[data-test-id='date'] input").setValue("3.06.2024");
+        $("[data-test-id='name'] input").setValue("Алексей Леонтьев");
+        $("[data-test-id='phone'] input").setValue("+72948764873");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
